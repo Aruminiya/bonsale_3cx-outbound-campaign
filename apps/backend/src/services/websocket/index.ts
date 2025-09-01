d6cea96fa5dc658514e6d05f3f84696b4beaa358
Aruminiya <leo80103@gmail.com>
2025-09-01 14:35:55 +0800

    feat: Delete unused files

--- apps/backend/src/services/websocket/index.ts
+++
@@ -1,8 +0,0 @@
-// WebSocket 服務統一導出
-export { create3CXWebSocket } from './create3CXWebSocket';
-export { 
-  roomWebSockets, 
-  cleanupConnection, 
-  isConnectionActive, 
-  getActiveConnections 
-} from './connectionManager';
